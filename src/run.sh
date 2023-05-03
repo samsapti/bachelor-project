5245ca05f412edd442d6e7d7ceef93af0cc5e14b
Sam Al-Sapti <sam@sapti.me>
2023-05-03 18:17:23 +0200

    Add -k to run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,11 @@
 
 cd MP-SPDZ
 
+if [[ $1 == "-k" ]]; then
+    killall semi-party.x
+    exit $?
+fi
+
 for i in $(seq 0 2); do
-    eval ".//semi-party.x -N 3 -p $i gale_shapley > ../out-p$i.txt 2>&1 &"
+    ./semi-party.x -N 3 -p "$i" gale_shapley > "../out-p$i.txt" 2>&1 &
 done
