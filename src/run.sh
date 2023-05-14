17377b483bf561961e4e33d952672296c33bfa12
Sam Al-Sapti <sam@sapti.me>
2023-05-15 00:07:42 +0200

    Edit run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")/MP-SPDZ" || exit 1
+PROG="semi2k-party.x"
 
 if [[ $1 == "-k" ]]; then
-    killall semi2k-party.x
-    exit $?
+    killall "$PROG"
+    exit "$?"
 fi
 
+cd "$(dirname "$0")/MP-SPDZ" || exit 1
+
 for i in $(seq 0 2); do
-    ./semi2k-party.x -v -N 3 -p "$i" -e gale_shapley > "../out-p$i.txt" 2>&1 &
+    "./$PROG" -v -N 3 -p "$i" -e gale_shapley > "../out-p$i.txt" 2>&1 &
 done
